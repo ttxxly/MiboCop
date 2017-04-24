d12e2e3788ec6e29cd13cce68140a414cc9fbaaa
ttxxly <ttxxly.gmail.com>
2017-04-24 17:06:46 +0800

    手机防盗功能已基本开发完毕。

---
+++ app/src/main/java/top/ttxxly/blog/mibocop/receiver/SmsReceiver.java
@@ -0,0 +1,16 @@
+package top.ttxxly.blog.mibocop.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ttxxly on 2017/4/24.
+ */
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
